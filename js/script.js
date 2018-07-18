f3483e7921d3bd5684addee08583c6b9f47b8711
qrames <ramque@orange.fr>
2018-07-18 14:21:51 +0200

    the card column finish

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Stuff to do as soon as the DOM is ready
+
+    // 
+    // var popper = new Popper(referenceElement, onPopper, {
+    //     placement: 'right'
+    // });
+
+
+});
